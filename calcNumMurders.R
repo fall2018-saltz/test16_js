a59040935da0c7a12351223c89555ce5d2898b07
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-03 18:20:56 -0400

    Saving calcNumMurders.R

--- calcNumMurders.R
+++ calcNumMurders.R
@@ -1,1 +1,7 @@
 
+# Adding a new column with a calulation for number of Murders
+df$numMurders <- df$population*df$Murder/100000
+str(df) # Take a look and see if you have a new column
+
+#See which state has the highest (#43 Texas)
+df[which.max(df$numMurders),] 

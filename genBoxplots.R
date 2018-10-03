c3d96b7073ff13d91bdf43bf441bd258d8578846
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-03 18:22:56 -0400

    Saving genBoxplots.R

--- genBoxplots.R
+++ genBoxplots.R
@@ -1,1 +1,8 @@
 
+# Boxplot for Population
+myboxPlotPop <- ggplot(df, aes(x=factor(0), y=population)) + geom_boxplot()
+boxPopulation <- myboxPlotPop
+
+# Boxplot for Murder
+myboxPlotMur <- ggplot(df, aes(x=factor(0), y=Murder)) + geom_boxplot()
+boxMurder <- myboxPlotMur

bc4b2b63e37a3d865d68470f95ae25001cffe3d5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-03 17:39:13 -0400

    Saving genHist.R

--- genHist.R
+++ genHist.R
@@ -1,6 +1,6 @@
 
 outTxt <- capture.output( str(df) )
 outTxt <- outTxt[1]
-outTxt
+outTxt <- paste("min population: " min(df$population), "  max population: ", max(df$population))
 str(df)
 histOut <- hist(df$population, breaks=20)

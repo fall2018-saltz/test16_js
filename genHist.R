403cb0830a4469b42bdfa9edd00f82ba597b78b3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-03 18:07:48 -0400

    Saving genHist.R

--- genHist.R
+++ genHist.R
@@ -1,7 +1,24 @@
 
-#outTxt <- capture.output( str(df) )
-#outTxt <- outTxt[1]
-outTxt <- paste("min population: ", min(df$population), "   max population: ", max(df$population))
-cat(outTxt)
-#str(df)
-histOut <- hist(df$population, breaks=20)
+# Histogram for Population
+myPlotPop <- ggplot(mergeDF, aes(x=population))
+myPlotPop <- myPlotPop + geom_histogram(binwidth=500000)    # think about what is a good bin width 
+myPlotPop <- myPlotPop + ggtitle("Histogram of population")
+histPopulation <- myPlotPop
+
+# Histogram for Murder rate
+myPlotMur <- ggplot(mergeDF, aes(x=Murder))
+myPlotMur <- myPlotMur + geom_histogram(binwidth=1)    # think about what is a good bin width 
+myPlotMur <- myPlotMur + ggtitle("Histogram of Murder Rate")
+histMurder <- myPlotMur
+
+# Histogram for Assault
+myPlotAst <- ggplot(mergeDF, aes(x=Assault))
+myPlotAst <- myPlotAst + geom_histogram(binwidth=100)    # think about what is a good bin width 
+myPlotAst <- myPlotAst + ggtitle("Histogram of Assaults")
+histAssaults <- myPlotAst
+
+# Histogram for Rape
+myPlotRape <- ggplot(mergeDF, aes(x=Rape))
+myPlotRape <- myPlotRape + geom_histogram(binwidth=1)    # think about what is a good bin width 
+myPlotRape <- myPlotRape + ggtitle("Histogram of # of Rapes")
+histRapes <- myPlotRape

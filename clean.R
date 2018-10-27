1e5fd044c846046ae1f836a89619bb4bede05e9a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-27 00:17:15 -0400

    force updated content when merge failed

--- clean.R
+++ clean.R
@@ -1,2 +1,25 @@
 
-cleanCensus <- raw_data
+#str(raw_data)
+
+#re-use the code from before
+readStates <- function(states)
+{
+  # remove rows that not needed
+  # -- first row is the total for the US, we do not need that
+  states <- states[-1,]
+  # -- last row is Puerto Rico, it's not a state
+  num.row <- nrow(states)
+  states <- states[-num.row,]
+  
+  #remove the first for columns
+  states <- states[,-1:-4]
+  
+  # change names for remaining columns
+  colnames(states)<-c("stateName", "population", "popOver18","percentOver18")
+  
+  # return the results
+  return(states)
+}
+
+cleanCensus <- readStates(raw_data)
+str(cleanCensus)

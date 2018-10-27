3f39d0e3108e070fe38c20ce658190f3b9a82240
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-27 00:17:17 -0400

    force updated content when merge failed

---
+++ genBoxplots.R
@@ -0,0 +1,9 @@
+
+library(ggplot2)
+# Boxplot for Population
+myboxPlotPop <- ggplot(df, aes(x=factor(0), y=population)) + geom_boxplot()
+boxPopulation <- myboxPlotPop
+
+# Boxplot for Murder
+myboxPlotMur <- ggplot(df, aes(x=factor(0), y=Murder)) + geom_boxplot()
+boxMurder <- myboxPlotMur
